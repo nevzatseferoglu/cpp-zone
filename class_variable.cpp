e039ae31c3a24e3a6de0cf192cea963c2b25f109
Nevzat Seferoglu <43863606+nevzatseferoglu@users.noreply.github.com>
2019-10-29 13:11:07 +0300

    Update class_variable.cpp

--- class_variable.cpp
+++ class_variable.cpp
@@ -8,14 +8,15 @@ class Person{
     Person(){};
     static const int getCount(){ return classCount;}
   private:
-    /* Default initialization of classCount class variable */
     static const int classCount = 3;
+    /* Default initialization of classCount class variable */
+    /* It is also possible but is not suitable for the purpose of using static variable */
 };
 int main(void)
 {
   cout << Person::getCount() << '\n'; // getCount has been already defined as class function;
   Person p1;
-  cout<< p1.getCount() << '\n'; // getCount function act like an instance function of the object but it is nor;
+  cout<< p1.getCount() << '\n'; // getCount function act like an instance function of the object but it is not;
 
   return 0;
 }
