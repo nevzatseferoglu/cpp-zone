7f9fc7699cc4d7653e3488c51be4cd25af39de77
Nevzat Seferoglu <43863606+nevzatseferoglu@users.noreply.github.com>
2019-11-25 21:49:55 +0300

    Update main.cpp

--- _Inheritance_Special_Function_Behaviour/main.cpp
+++ _Inheritance_Special_Function_Behaviour/main.cpp
@@ -6,11 +6,11 @@ using namespace As;
 int main(void)
 {
     std::cout << "\nType 1 ====================\n\n";
-    A a3(1);
+    A a1(1);
     std::cout << "\nType 2 ====================\n\n";
     A a2(1,1);
     std::cout << "\nType 3 ====================\n\n";
-    A a1(1,1,1);
+    A a3(1,1,1);
     std::cout << "===========================\n\n";
 
     return 0;
